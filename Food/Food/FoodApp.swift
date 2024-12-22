d8dffc408090d8646911f6e293e1bebf0476ff7a
omer cakar <omerrccak@gmail.com>
2024-12-22 08:51:59 +0300

    project launch

---
+++ Food/Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by ÖMER  on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
